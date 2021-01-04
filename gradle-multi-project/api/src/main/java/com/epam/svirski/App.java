2b32fb6bee1bc72fd93a3a7451102bfc23517ff4
Kostya <kostyashtc@gmail.com>
2021-01-04 17:31:36 +0300

    feat: add multiproject

---
+++ gradle-multi-project/api/src/main/java/com/epam/svirski/App.java
@@ -0,0 +1,10 @@
+package com.epam.svirski;
+
+public class App {
+
+	public static void main(String[] args) {
+		boolean result = Utils.isAllPositiveNumbers("-12", "79");
+		System.out.println(result);
+	}
+
+}
